e4c3a61299814b0c1898bb7fccdcb095e2260f17
adamjad <adam.charif.git@gmail.com>
2016-11-25 00:12:41 +0100

    ajouter dbConn

---
+++ src/DAO/UserDAO.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Adam
+ * Date: 25/11/2016
+ * Time: 00:02
+ */
+class UserDAO
+{
+
+}
--- src/DbConn.php
+++ src/DbConn.php
@@ -6,7 +6,109 @@
  * Date: 24/11/2016
  * Time: 21:37
  */
+
 class DbConn
 {
+    /**
+     * Instance de la classe SPDO
+     *
+     * @var DbConn
+     * @access private
+     */
+    private $PDOInstance = null;
 
+    /**
+     * Instance de la classe SPDO
+     *
+     * @var DbConn
+     * @access private
+     * @static
+     */
+    private static $instance = null;
+
+    /**
+     * Constante: nom d'utilisateur de la bdd
+     *
+     * @var string
+     */
+    const USER = 'root';
+
+    /**
+     * Constante: hôte de la bdd
+     *
+     * @var string
+     */
+    const HOST = 'localhost';
+
+    /**
+     * Constante: hôte de la bdd
+     *
+     * @var string
+     */
+    const PASS = '';
+
+    /**
+     * Constante: nom de la bdd
+     *
+     * @var string
+     */
+    const DB = 'db_SciMs';
+
+    /**
+     * Constructeur
+     *
+     * @param void
+     * @return void
+     * @see PDO::__construct()
+     */
+    private function __construct()
+    {
+        try {
+            $this->_pdo = new PDO('mysql:dbname=' . self::DB . ';host=' . self::HOST, self::USER, self::PASS);
+            $this->_pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (Exception $e) {
+            echo 'Erreur : ' . $e->getMessage() . '<br />';
+            die();
+        }
+    }
+
+    /**
+     * Crée et retourne l'objet
+     *
+     * @access public
+     * @static
+     * @param void
+     * @return DbConn $instance
+     */
+    public static function getInstance()
+    {
+        if (is_null(self::$instance)) {
+            self::$instance = new DbConn();
+        }
+        return self::$instance;
+    }
+
+    /**
+     * Exécute une requête SQL avec PDO
+     *
+     * @param string $query La requête SQL
+     * @return PDOStatement Retourne l'objet PDOStatement
+     */
+    public function query($query)
+    {
+        return $this->PDOInstance->query($query);
+    }
+
+    /**
+     * Exécute une requête SQL préparée avec PDO
+     *
+     * @param string $query La requête SQL
+     * @param array $statement template de requête SQL valide pour le serveur de base de données cible
+     * @return PDOStatement Retourne l'objet PDOStatement
+     */
+    public function execute($query, array $statement) {
+        $request = $this->PDOInstance->prepare($query);
+        return $request->execute($statement);
+
+    }
 }
